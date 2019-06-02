0fa25fe6f68b6b723eeb582d60624f6f38f6b238
DESKTOP-CI66081\Sergiu <sergiugroza1997@yahoo.com>
2019-06-02 18:30:03 +0300

    Add project files.

---
+++ APPs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APPs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
